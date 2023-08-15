751433def236ef8210afd2efdc075c5687db746d
Phantom-d-e-v <atharvashedge@gmail.com>
2023-08-15 18:13:05 +0530

    Feat: Meet panel done

---
+++ frontend/src/routes/jobs/+page.svelte
@@ -0,0 +1,159 @@
+<script>
+import jobsHero from '$lib/jobsHero.png';
+import edgeEx from '$lib/edgeEx.png';
+import embedGeek from '$lib/embedGeek.png';
+import fullstackJug from '$lib/fullstackJug.png';
+import aimlHust from '$lib/aimlHust.png';
+import uiuxartist from '$lib/uiuxartist.png';
+import jobsApply from '$lib/jobsApply.png';
+import GradientButton from '$lib/components/GradientButton.svelte';
+</script>
+
+<section
+	style=" background-image: url('{jobsHero}'); "
+	class="min-h-[calc(777px)] w-full flex-col items-center justify-end bg-cover bg-center bg-no-repeat p-3 md:p-12">
+	<div class=" mt-[431px] flex w-full items-end justify-center p-3 align-bottom">
+		<h3 class="text-5xl font-semibold text-base-100">Come Join our Team</h3>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{edgeEx}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-left md:w-2/3 md:items-start">
+			<h4 class="mb-6 text-4xl font-semibold text-secondary md:mb-14">
+				Edge <span class="text-info">Explorer</span>
+			</h4>
+
+			<p
+				class="w-fit bg-gradient-to-l from-secondary to-info bg-clip-text text-lg font-medium text-transparent">
+				Small is beautiful... Edge is sexy !!
+			</p>
+
+			<p class="mt-5 text-neutral/60">
+				I build world-class applications in constrained hardware. Data engineering, processing,
+				analytics, privacy & security, visualization & communication that's all in a day's work for
+				me. My latest passion is exploring AI/ML/Tiny ML at the edge. Believe me anything that can
+				be done on the cloud, can be done better at the edge !
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div
+		class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row-reverse md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{embedGeek}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-left md:w-2/3 md:items-start">
+			<h4 class="mb-6 text-4xl font-semibold text-secondary md:mb-14">
+				Embedded <span class="text-info">Geek</span>
+			</h4>
+
+			<p
+				class="w-fit bg-gradient-to-l from-secondary to-info bg-clip-text text-lg font-medium text-transparent">
+				I am the king of firmware !
+			</p>
+
+			<p class="mt-5 text-neutral/60">
+				Bring any hardware, & I can build the best applications on it. My goal is to build
+				everything great about cloud (micro services based architectures, modularity,
+				configurability...) and more to the firmware world. I make boring devices 'SOCIAL' (Secure,
+				Open, Connected, Intelligent, Autonomous, Light)
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{fullstackJug}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-left md:w-2/3 md:items-start">
+			<h4 class="mb-6 text-4xl font-semibold text-secondary md:mb-14">
+				Full Stack <span class="text-info">Juggler</span>
+			</h4>
+
+			<p
+				class="w-fit bg-gradient-to-l from-secondary to-info bg-clip-text text-lg font-medium text-transparent">
+				I'm the complete package !
+			</p>
+
+			<p class="mt-5 text-neutral/60">
+				Want to build a business logic? Want to define data logic? Want to develop intuitive
+				visuals? Want to design, develop, integrate & deploy full stack programs? FOr all of the
+				above, I'm the man for you oh, & I'm not racist when it comes to languages - it's all ones &
+				zeros ;)
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div
+		class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row-reverse md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{aimlHust}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-left md:w-2/3 md:items-start">
+			<h4 class="mb-6 text-4xl font-semibold text-secondary md:mb-14">
+				AI/ML <span class="text-info">Hustler</span>
+			</h4>
+
+			<p
+				class="w-fit bg-gradient-to-l from-secondary to-info bg-clip-text text-lg font-medium text-transparent">
+				If AI is the future, then the future is here !
+			</p>
+
+			<p class="mt-5 text-neutral/60">
+				I just love it when my algos can beat a human at something she/he is best at. & that's why I
+				like to hustle from design to coding to deployment of my algo babies. My ML models can
+				replicate any math formula, can communicate on text or audio, & can play with images or
+				video. Basically if you treat me like Iron Man, I can build you a JARVIS ;)
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{uiuxartist}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-left md:w-2/3 md:items-start">
+			<h4 class="mb-6 text-4xl font-semibold text-secondary md:mb-14">
+				UI/UX <span class="text-info">Artist</span>
+			</h4>
+
+			<p
+				class="w-fit bg-gradient-to-l from-secondary to-info bg-clip-text text-lg font-medium text-transparent">
+				Picasso... vanGogh... Me !
+			</p>
+
+			<p class="mt-5 text-neutral/60">
+				Getting a device/program/application to look good, feel good, & interact with a human in the
+				most intuitive way - that's what makes me an artist of the tech world. It's a process : the
+				storyboards, the quick but smooth prototypes & finally the masterpiece...a piece of art
+				disguised as an app on your phone !
+			</p>
+		</div>
+	</div>
+</section>
+
+<section
+	style=" background-image: url('{jobsApply}'); "
+	class=" m-auto flex h-[500px] content-end items-center justify-end bg-cover bg-center bg-no-repeat p-3 text-left md:p-12">
+	<div class=" flex w-full flex-col p-3 md:w-1/3">
+		<h3 class="text-5xl font-semibold text-base-100">There’s room for more…</h3>
+		<p class="mt-10 max-w-3xl text-xl text-base-100">
+			So if you find yourself compelled to be part of adventure -
+		</p>
+		<div class="mt-10 w-fit">
+			<GradientButton>sadfsdaf</GradientButton>
+		</div>
+	</div>
+</section>
---
+++ frontend/src/routes/ourcompany/+page.svelte
@@ -0,0 +1,256 @@
+<script>
+import aboutHero from '$lib/aboutHero.png';
+import GradientButton from '$lib/components/GradientButton.svelte';
+import karthik from '$lib/Karthik.png';
+import prakash from '$lib/Prakash.png';
+import karthikIR from '$lib/KarthikIR.png';
+import anoop from '$lib/Anoop 1.png';
+import gradientbackground from '$lib/gradient_background.png';
+import valuesWe from '$lib/Values We Live By.png';
+</script>
+
+<section
+	style=" background-image: url('{aboutHero}'); "
+	class="flex h-[777px] w-full flex-col items-center justify-center bg-cover bg-center bg-no-repeat p-3 md:p-12">
+	<div class=" flex flex-col items-center p-3 text-center align-bottom">
+		<h3 class=" content-center text-6xl text-base-100">
+			Building Tomorrow<br /><span class="font-bold">@ the Edge</span>
+		</h3>
+
+		<p class="mt-16 max-w-3xl text-center text-xl text-base-100">
+			Copy Humans are creators… & just as no two humans are alike, no two devices they create, or
+			the environment they exist in, are alike! Consequently, each device has [& deserves] its own
+			identity...and an ecosystem that enables an adaptive system of interactive experience between
+			man & machine is the future.
+		</p>
+		<p class="mt-2 max-w-3xl text-center text-xl text-base-100">
+			TinkerBloX is born out of the ambition to curate and orchestrate this world of adaptive
+			systems of devices, sustainably… a world that empowers devices to rise above the clouds…
+		</p>
+		<h3 class=" mt-10 text-3xl font-semibold text-base-100">A world at the edge of tomorrow!</h3>
+		<div class=" mt-16 w-fit">
+			<GradientButton>LEARN MORE</GradientButton>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{karthik}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-center md:w-2/3">
+			<h4 class=" text-4xl font-semibold text-secondary">
+				<h4 class=" text-4xl font-semibold text-secondary">
+					<span class="text-info">karthik </span><span class="text-gray-600"> ‘G.K.’</span><br />
+					gopalakrishnan
+				</h4>
+			</h4>
+
+			<p class=" mt-20 w-fit text-4xl text-neutral/60">Chief Executive Officer</p>
+
+			<p class="mt-16 w-96 text-neutral/60">
+				IoT visionary  |  Magician at scaling business  | Great party host | Avid boxer | Doting
+				father  |  Confused husband …
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex w-11/12 flex-col items-center text-center md:w-2/3">
+		<h4 class="mb-6 text-4xl text-secondary md:mb-14">
+			Sequencing <span class="font-semibold text-info"> Our DNA…</span>
+		</h4>
+
+		<p class="mt-5 text-neutral/60">
+			<span class="text-secondary">@ TinkerBloX</span>, each day is a step towards curating the
+			connected device habitat that helps you realize “true real-time” efficiency for your
+			operations. Our AI + IoT building blocks empower enterprises to experience the cause & effect
+			continuum at the actual site of action…the Edge ! & as such, it is in our DNA to deliver
+			Device-Native-AIoT in configurable plug-&-play blocks that are sustainable by design and will
+			always give you the edge…
+		</p>
+
+		<p class="mt-5 text-info">
+			If you are as excited as we are at the prospect of a truly Phygital world driven by
+			device-centric experience & the servitization economy, check out our transformation blocks…
+		</p>
+
+		<p
+			class=" mt-16 w-fit bg-gradient-to-l from-secondary to-info bg-clip-text text-4xl font-medium text-transparent">
+			SandBlox | EdgeBlox  | ServBlox
+		</p>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div
+		class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row-reverse md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{anoop}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-center md:w-2/3">
+			<h4 class=" text-4xl font-semibold text-secondary">
+				<h4 class=" text-4xl font-semibold text-secondary">
+					<span class="text-info">anoop </span><span class="text-gray-600"> ‘A.B.C’</span><br />
+					balachandran
+				</h4>
+			</h4>
+
+			<p class=" mt-20 w-fit text-4xl text-neutral/60">Chief Edge-er</p>
+
+			<p class="mt-16 w-96 text-neutral/60">
+				AIoT evangelist  |  the “big picture” guy  | ardent foodie |  tourist by profession | mostly
+				single …
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex w-11/12 flex-col items-center text-center md:w-2/3">
+		<h4 class="mb-6 text-4xl text-secondary md:mb-14">
+			Believe in the <span class="font-semibold text-info"> edge…</span>
+		</h4>
+
+		<p class="mt-5 text-neutral/60">
+			To build a new world of autonomous device ecosystems…<br />
+			To work tirelessly on the belief that true distributed intelligence is possible at the edge…<br />
+			To orchestrate a truly seamless experience that enables interaction between every device and its
+			human…<br />
+		</p>
+
+		<p
+			class=" mt-5 bg-gradient-to-l from-secondary to-info bg-clip-text text-lg font-medium text-transparent">
+			and to do all this while driving home tangible business outcomes for our clients & partners
+			every step of the way…sustainably…
+		</p>
+
+		<p class=" mt-16 w-fit text-3xl font-semibold">
+			<span class="text-info">…That is the </span>purpose of
+			<span class="text-secondary">TinkerBloX</span>
+		</p>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{karthikIR}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-center md:w-2/3">
+			<h4 class=" text-4xl font-semibold text-secondary">
+				<h4 class=" text-4xl font-semibold text-secondary">
+					<span class="text-info">karthik </span><span class="text-gray-600"> ‘IRK’</span><br />
+					rangasamy
+				</h4>
+			</h4>
+
+			<p class=" mt-20 w-fit text-4xl text-neutral/60">Chief Build-er</p>
+
+			<p class="mt-16 w-96 text-neutral/60">
+				digital geek  |  nurtures great teams  | coffee connoisseur  |  chef crafting the perfect
+				recipes - both food & software
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex w-11/12 flex-col items-center text-center md:w-2/3">
+		<h4 class="mb-6 text-4xl text-secondary md:mb-14">
+			We’ve got “just” what you <span class="font-semibold text-info"> need... </span>
+		</h4>
+
+		<p class="mt-5 text-neutral/60">
+			The world has fooled us into thinking that we need to pick from what’s available, and live
+			with it…just accept the shortcomings…pay for features we’d never use…be happy with the
+			half-baked outcomes…because “that’s what you get” ! Not anymore…<br /><span
+				class="text-secondary">@ TinkerBloX</span
+			>@ TinkerBloX, we’ve made it our mission to enable businesses with the power of tech with full
+			freedom to build the solution exactly as you’d want to; & a full set of services from
+			consulting to implementation to ensure you get nothing short of your desired outcome!
+		</p>
+
+		<p class="mt-5 text-info">
+			& for our focus verticals, this comes with our promise for Value-Of-Investment : a promise of
+			needing to spend on only what you need; a promise of outcomes from pilot → scale → service
+			delivery aaS
+		</p>
+
+		<p
+			class=" mt-16 w-fit bg-gradient-to-l from-secondary to-info bg-clip-text text-4xl font-medium text-transparent">
+			Check out our Super BloX : <br />
+			Smart Spaces | Smart Devices  | Smart Utilities | Smart Industry
+		</p>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div
+		class="m-auto flex max-w-6xl flex-col items-start gap-14 md:flex-row-reverse md:items-center">
+		<div class="m-auto w-2/3 md:w-1/3">
+			<img src="{prakash}" alt="" />
+		</div>
+		<div class="m-auto flex w-11/12 flex-col items-center text-center md:w-2/3">
+			<h4 class=" text-4xl font-semibold text-secondary">
+				<h4 class=" text-4xl font-semibold text-secondary">
+					<span class="text-info">prakash </span>
+					G.R.
+				</h4>
+			</h4>
+
+			<p class=" mt-20 w-fit text-4xl text-neutral/60">Chief Builder</p>
+
+			<p class="mt-16 w-96 text-neutral/60">
+				Farmer; Engineering Leader; Continuous Learner & Problem Solver; Makes money in Software :
+				spends some of it in farming and loses the rest in stock markets
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class=" w-full py-16">
+	<div class="m-auto flex w-11/12 flex-col items-center text-center md:w-2/3">
+		<h4 class="mb-6 text-4xl font-semibold text-secondary md:mb-14">
+			Democratizing <span class=" text-info"> Tech…</span>
+		</h4>
+
+		<p class="mt-5 text-neutral/60">
+			A true techie isn’t happy with building tech products ; he wants to be part of a family of
+			creators… <br />A true techie doesn’t hoard power; he empowers his fellows to tinker around
+			with his creation & make it better… <br />A true techie is cognizant of security, but works
+			towards building an open & sharing ecosystem…
+		</p>
+
+		<p
+			class=" mt-5 bg-gradient-to-l from-secondary to-info bg-clip-text text-lg font-medium text-transparent">
+			With some very passionate Tech-“BloXers”, TinkerBloX isn’t a company that sells tech ; We are
+			the first step to a community that brings tomorrow’s tech to life.
+		</p>
+
+		<p class=" mt-16 w-fit text-3xl font-semibold">
+			May the Edge
+			<span class="text-secondary">be with you !!</span>
+		</p>
+	</div>
+</section>
+
+<section
+	style=" background-image: url({gradientbackground}) "
+	class=" min-h-16 w-full bg-primary bg-cover bg-bottom bg-no-repeat py-10">
+	<div class="m-auto max-w-7xl">
+		<h4 class=" mb-16 w-full text-center text-5xl text-base-100">Values we live by…</h4>
+		<p class="mt-16 max-w-3xl text-center text-xl text-base-100">
+			A new world needs to update to some new values while at the same time holding on to some
+			traditional ones… Here’s what we live by through thick & thin…
+		</p>
+		<div class="min-h-16 mt-5 w-full">
+			<img
+				src="{valuesWe}"
+				alt="values we live by"
+				class=" h-auto w-full object-contain object-center" />
+		</div>
+	</div>
+</section>
