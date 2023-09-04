1098ea54b51dd5f8f47a9de69c047ce5733636f8
glitchnobody <58382686+glitchnobody@users.noreply.github.com>
2023-09-04 22:00:06 +0530

    started smartspaces

--- frontend/src/routes/superblox/smartspaces/+page.svelte
+++ frontend/src/routes/superblox/smartspaces/+page.svelte
@@ -1,20 +1,39 @@
 <script>
-import comingsoon from '$lib/comingsoon.png';
-import GradientButton from '$lib/components/GradientButton.svelte';
+import SmartSpaces from '$lib/smartspaces_hero.png';
 </script>
 
 <section
-	style="  background-image: url('{comingsoon}'); "
+	style="  background-image: url('{SmartSpaces}'); "
 	class="relative z-30 flex h-fit min-h-[777px] w-full flex-col items-center justify-center bg-cover bg-center bg-no-repeat p-3 py-5 md:p-12">
-	<div class=" flex flex-col items-center p-3 text-center align-bottom">
-		<h3 class=" content-center text-4xl font-bold text-base-100 md:text-7xl">Coming Soon…</h3>
+</section>
 
-		<p class="mt-16 max-w-3xl text-center text-xl text-base-100 md:text-2xl">
-			Dear Techies, this page is still in development and will be launched soon.<br />Stay tuned for
-			our updates.
+<section
+	style="background-image: url(/defaultbg.png);"
+	class=" min-h-[561px] w-full bg-cover bg-center bg-no-repeat py-16">
+	<div class="m-auto flex min-h-[561px] max-w-6xl flex-col items-center gap-2">
+		<h4 class=" w-3/5 text-center text-4xl text-secondary">
+			Take the next step in your <span class=" font-bold text-info">smart spaces</span> journey…
+			<span class="text-accent"> one BloX</span> @ a time !!
+		</h4>
+
+		<p class="mt-5 text-center text-lg text-neutral/60">
+			There used to be a time when the spaces we lived in, travelled in, worked in and played in,
+			were only that – spaces.  Interactions, if any, were limited, unidirectional and mostly,
+			boring. <br /> <br /> With digital technologies becoming mainstream, this has changed
+			drastically. Today, it’s not unimaginable for homes to interact with its inhabitants, for
+			offices to proactively care for the environment of its workers, for factories to take
+			responsibility of the safety & security of its men & machines, for greenhouses to adapt to the
+			needs of its plant life, and for all of the above to be conscious of their role in creating a
+			sustainable and eco-friendly environment <br /> <br />
+			<span
+				class=" m-auto w-fit bg-gradient-to-l from-secondary to-info bg-clip-text p-5 pb-5 text-center font-medium text-info text-transparent">
+				@TinkerBloX, we believe in creating SAFE – Sustainable, Autonomous, Federated, Efficient –
+				spaces for all of the aforementioned scenarios leveraging on our Device Native Edge-Centric
+				suite of AIoT products…
+			</span>
 		</p>
-		<div class=" mt-16 w-fit">
-			<GradientButton>LEARN MORE</GradientButton>
-		</div>
+	</div>
+	<div class=" sticky top-16 h-4 w-full bg-gray-400">
+		<div></div>
 	</div>
 </section>
